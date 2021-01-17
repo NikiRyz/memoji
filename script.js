704a954f6d12884a52d6f9f12bc06c55da3af54a
Рязанова Вероника Кирилловна <VKRyazanova@sberbank.ru>
2021-01-17 20:59:58 +1000

    Добавила смайлики

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+function coupleArr(arr) {
+    return [].concat(arr, arr);
+  }
+  function shuffleArr (arr){
+      return arr.sort(()=>Math.random()-0.5)
+  }  
 (function init() {
+    const emoji = '🤓 😍 😂 😊 😆 😏'.split(' ');
+    const shuffleEmoji  = shuffleArr(coupleArr(emoji))
     const cards = Array.from(document.querySelectorAll('.card'))
+    fillCards(cards,shuffleEmoji)
     cards.forEach((card) => { card.addEventListener('click', clickEvent) })
 }())
-
+function fillCards(cards,emoji){
+    cards.forEach((card,index)=>{card.textContent=emoji[index]})
+}
 function clickEvent() {
     const opened = this.classList.contains('open')
     if (!opened) {
